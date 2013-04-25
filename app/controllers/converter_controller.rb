5f58245445c4c763f731fc2274ae4d435b2c8bac
João Daniel <jdanielnd@gmail.com>
2013-04-25 19:55:12 -0300

    rolling back config of path

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,7 +3,6 @@ class ConverterController < ApplicationController
     name = params[:filename]
     html = params[:html]
     options = params[:options] || {}
-    options = options.merge({:wkhtmltopdf => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s})
     pdf = WickedPdf.new.pdf_from_string(html, options)
     send_data(pdf, :filename => "#{name}.pdf", :type => "application/pdf")
   end
