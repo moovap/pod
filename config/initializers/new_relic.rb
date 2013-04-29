3854a126564f1109a9c61820acbbc7e230419694
João Daniel <jdanielnd@gmail.com>
2013-04-29 18:59:18 -0300

    changing includes

--- config/initializers/new_relic.rb
+++ config/initializers/new_relic.rb
@@ -1,13 +1,8 @@
 # Ensure the agent is started using Unicorn.
 # This is needed when using Unicorn and preload_app is not set to true.
 # See https://newrelic.com/docs/ruby/no-data-with-unicorn
-if defined? NewRelic
-  class ActionController::API
-    include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-    include NewRelic::Agent::Instrumentation::Rails3::ActionController
-  end
-end
 if defined? Unicorn
+  include NewRelic::Agent
   ::NewRelic::Agent.manual_start()
   ::NewRelic::Agent.after_fork(:force_reconnect => true)
 end
