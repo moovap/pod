565525b6daf2d3a7f923c3d12632956b3d2f7b08
João Daniel <jdanielnd@gmail.com>
2013-04-29 19:07:22 -0300

    changing new_relic.rb initializer

--- config/initializers/new_relic.rb
+++ config/initializers/new_relic.rb
@@ -1,8 +1,8 @@
 # Ensure the agent is started using Unicorn.
 # This is needed when using Unicorn and preload_app is not set to true.
 # See https://newrelic.com/docs/ruby/no-data-with-unicorn
-if defined? Unicorn
-  include NewRelic::Agent
-  ::NewRelic::Agent.manual_start()
-  ::NewRelic::Agent.after_fork(:force_reconnect => true)
-end
+# if defined? Unicorn
+#   include NewRelic::Agent
+#   ::NewRelic::Agent.manual_start()
+#   ::NewRelic::Agent.after_fork(:force_reconnect => true)
+# end
