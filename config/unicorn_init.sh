7fdaac8e22b9c628ae64f00e11d42eb363a25d28
João Daniel <jdanielnd@gmail.com>
2013-04-29 16:52:49 -0300

    reverting config changes

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -4,8 +4,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/pod/current
-# PID=$APP_ROOT/tmp/pids/unicorn.pid
-PID=/home/deployer/apps/pod/shared/tmp/pids/unicorn.pid
+PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
 set -u
