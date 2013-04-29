c534af60f7cb02e35098a3692aeb9336a86259c0
João Daniel <jdanielnd@gmail.com>
2013-04-29 18:52:41 -0300

    adding NewRelic includes to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include NewRelic::Agent::Instrumentation::Rails3::ActionController
   protect_from_forgery
 end
