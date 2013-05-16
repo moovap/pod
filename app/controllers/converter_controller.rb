9c0a4024cdb5e1eaad6d2985d0faca5eab0c0bd6
João Daniel <jdanielnd@gmail.com>
2013-05-16 19:29:05 -0300

    removing redirect and js delay to 16000

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,7 +3,7 @@ class ConverterController < ApplicationController
     name = params[:filename]
     html = params[:html]
     options = params[:options] || {}
-    options = options.merge({use_xserver: true, javascript_delay: 8000, redirect_delay: 8000})
+    options = options.merge({use_xserver: true, javascript_delay: 16000})
     pdf = WickedPdf.new.pdf_from_string(html, options)
     send_data(pdf, :filename => "#{name}.pdf", :type => "application/pdf")
   end
