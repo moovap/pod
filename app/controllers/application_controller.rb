0c98672d14827039823b836ce5d996716c256403
João Daniel <jdanielnd@gmail.com>
2013-05-16 19:55:06 -0300

    filtering html and js delay to 6000

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-  include NewRelic::Agent::Instrumentation::Rails3::ActionController
   protect_from_forgery
 end
