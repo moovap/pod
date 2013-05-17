f516fc7c952149e337bfedbe803086447e01395c
João Daniel <jdanielnd@gmail.com>
2013-05-17 12:52:28 -0300

    removing gzip

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,9 +1,7 @@
 class ConverterController < ApplicationController
   def convert
     name = params[:filename]
-    # html = params[:html]
-    compressed_html = params[:html]
-    html = ActiveSupport::Gzip.decompress(compressed_html)
+    html = params[:html]
     options = params[:options] || {}
     options = options.merge({use_xserver: true, javascript_delay: 5000})
     pdf = WickedPdf.new.pdf_from_string(html, options)
