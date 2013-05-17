e168fa583c7eb179c8164945bc3accba60121bde
João Daniel <jdanielnd@gmail.com>
2013-05-17 11:42:31 -0300

    fixing gzip parameter

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -2,7 +2,7 @@ class ConverterController < ApplicationController
   def convert
     name = params[:filename]
     compressed_html = params[:html]
-    html = ActiveSupport::Gzip::Stream.decompress(compressed_html)
+    html = ActiveSupport::Gzip.decompress(compressed_html)
     options = params[:options] || {}
     options = options.merge({use_xserver: true, javascript_delay: 3000})
     pdf = WickedPdf.new.pdf_from_string(html, options)
