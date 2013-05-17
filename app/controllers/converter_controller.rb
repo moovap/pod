584bc0f5cf5405299789dec2a58a728f76ae58e6
João Daniel <jdanielnd@gmail.com>
2013-05-17 10:11:20 -0300

    moving js delay to sh

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,7 +3,7 @@ class ConverterController < ApplicationController
     name = params[:filename]
     html = params[:html]
     options = params[:options] || {}
-    options = options.merge({use_xserver: true, javascript_delay: 6000})
+    options = options.merge({use_xserver: true})
     pdf = WickedPdf.new.pdf_from_string(html, options)
     send_data(pdf, :filename => "#{name}.pdf", :type => "application/pdf")
   end
