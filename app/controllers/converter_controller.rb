862fa30a767955bf3c1757902c3f47795bc65595
João Daniel <jdanielnd@gmail.com>
2013-05-17 10:50:38 -0300

    delay config back to controller

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,7 +3,7 @@ class ConverterController < ApplicationController
     name = params[:filename]
     html = params[:html]
     options = params[:options] || {}
-    options = options.merge({use_xserver: true})
+    options = options.merge({use_xserver: true, javascript_delay: 8000})
     pdf = WickedPdf.new.pdf_from_string(html, options)
     send_data(pdf, :filename => "#{name}.pdf", :type => "application/pdf")
   end
