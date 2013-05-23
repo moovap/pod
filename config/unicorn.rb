f6a6371913ec0b63a053e1a2d2a35786de5a922e
João Daniel <jdanielnd@gmail.com>
2013-05-23 15:38:29 -0300

    changing log dir

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,8 +2,8 @@ app_name = "pod"
 root = "/home/deployer/apps/#{app_name}/current"
 working_directory root
 pid "/var/run/unicorn/unicorn_#{app_name}.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "/var/run/unicorn/log/unicorn_#{app_name}.log"
+stdout_path "/var/run/unicorn/log/unicorn_#{app_name}.log"
 
 listen "/tmp/unicorn.#{app_name}.sock"
 worker_processes 2
