7347a9d3a8e62c020c74d96b06d46a3d1eea093b
João Daniel <jdanielnd@gmail.com>
2013-05-23 15:28:18 -0300

    changing pid dir

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 app_name = "pod"
 root = "/home/deployer/apps/#{app_name}/current"
 working_directory root
-pid "/var/run/unicorn_#{app_name}.pid"
+pid "/var/run/unicorn/unicorn_#{app_name}.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
