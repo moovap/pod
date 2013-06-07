2e3cce6d8db201414378e14eb049d6b4426f471f
João Daniel <jdanielnd@gmail.com>
2013-06-07 10:54:15 -0300

    adding metadata

---
+++ chef/cookbooks/pod/metadata.rb
@@ -0,0 +1,20 @@
+name             'pod'
+maintainer       'Tecto'
+maintainer_email 'jd@fores.ee'
+license          'MIT'
+description      'Installs/Configures pod'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
+depends "build-essential"
+depends "ohai"
+depends "rbenv"
+depends "rbenv-bundler"
+depends "ruby_build"
+depends "runit"
+depends "sudo"
+depends "tar"
+depends "unicorn"
+depends "xvfb"
+depends "yum"
